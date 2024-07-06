16bdb278b6ece3c7f2ad2aed0cd4e9cd3ef81c8a
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2024-07-06 18:47:40 -0400

    Se agrega dialectModule: mysql2 a db.js

--- data/db.js
+++ data/db.js
@@ -1,10 +1,12 @@
 const { Sequelize } = require("sequelize")
+const mysql2 = require('mysql2');
 
 // nombre de la base datos - user - contraseña - { donde esta alojada tu bd?, lenguaje, puerto }
 
 const db = new Sequelize("leom_empresa_db", "leom", "grupo25api", {
     host: "mysql-leom.alwaysdata.net",
     dialect: "mysql",
+    dialectModule: mysql2,
     port: 3306
 })
 
