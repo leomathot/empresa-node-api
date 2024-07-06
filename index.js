559cb777c4bf9f741145e4305f5ebfa9feb51b85
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2024-07-06 00:57:19 -0400

    Se cambia PORT en index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const PORT = 3030
+const PORT = process.env.PORT || 3030
 const cors = require("cors")
 const empleadosRouter = require("./routes/empleadosRouter.js")
 const db = require ("./data/db.js")
