69d2efad8a588e45fcc7cb23f275a1e685fe5b25
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2024-07-06 00:51:09 -0400

    Se configura db

--- data/db.js
+++ data/db.js
@@ -2,8 +2,8 @@ const { Sequelize } = require("sequelize")
 
 // nombre de la base datos - user - contraseña - { donde esta alojada tu bd?, lenguaje, puerto }
 
-const db = new Sequelize("empresa", "root", "", {
-    host: "localhost",
+const db = new Sequelize("leom_empresa_db", "leom", "grupo25api", {
+    host: "mysql-leom.alwaysdata.net",
     dialect: "mysql",
     port: 3306
 })
