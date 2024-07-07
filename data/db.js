8196212a30d9f4305e4ebfdc7b978cf72693800b
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2024-07-06 21:16:56 -0400

    Se agrega .env

--- data/db.js
+++ data/db.js
@@ -3,8 +3,8 @@ const mysql2 = require('mysql2');
 
 // nombre de la base datos - user - contraseña - { donde esta alojada tu bd?, lenguaje, puerto }
 
-const db = new Sequelize("leom_empresa_db", "leom", "grupo25api", {
-    host: "mysql-leom.alwaysdata.net",
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: "mysql",
     dialectModule: mysql2,
     port: 3306
